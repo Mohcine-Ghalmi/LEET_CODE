0372dd10d479a5bb1e93dfa4d00961fee1e561b1
Mohcine Ghalmi <mghalmi@e3r3p1.1337.ma>
2024-03-13 23:39:33 +0000

    383. Ransom Note

--- 383. Ransom Note/Solution.cpp
+++ 383. Ransom Note/Solution.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
+        int j = 0;
         for (int i = 0; i < ransomNote.length(); i++)
-            if (ransomNote[i] != magazine[i])
-                return (false);
-        return (true);
+            for (int k = 0; k < magazine.length(); k++)
+                if (ransomNote[i] == magazine[k]) {
+                    magazine.erase(k, 1);
+                    j++;
+                    break;
+                }
+        if (j == ransomNote.length())
+            return (true);
+        return (false);
     }
 };

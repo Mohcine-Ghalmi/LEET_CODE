dd74a09bc639808bb5869d4ae05e21f56d5ea286
Mohcine Ghalmi <mghalmi@e3r2p17.1337.ma>
2024-10-12 17:21:13 +0100

    solution 23. Merge k Sorted Lists

---
+++ 23. Merge k Sorted Lists/Soluion.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+void addNodeAtEnd(ListNode* &head, int value) {
+    ListNode* newNode = new ListNode(value);
+    
+    if (head == nullptr)
+        head = newNode;
+    else {
+        ListNode* tmp = head;
+        while (tmp->next != nullptr) 
+            tmp = tmp->next;
+        tmp->next = newNode;
+    }
+}
+
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    vector<int> vec;
+    ListNode* newList = new ListNode(0);
+
+    while (list1) {
+        vec.push_back(list1->val);
+        list1 = list1->next;
+    }
+
+    while (list2) {
+        vec.push_back(list2->val);
+        list2 = list2->next;
+    }
+    sort(vec.begin(), vec.end());
+    for (auto number : vec)
+        addNodeAtEnd(newList, number);
+    newList = newList->next;
+    
+    return newList;
+}
+
+ListNode* mergeKLists(vector<ListNode*>& lists) {
+    ListNode *newList = nullptr;
+    vector<int> sortedVect;
+
+    for (ListNode *list : lists)
+        while (list) {
+            sortedVect.push_back(list->val);
+            list = list->next;
+        }
+    
+    sort(sortedVect.begin(), sortedVect.end());
+    
+    for (int val : sortedVect)
+        addNodeAtEnd(newList, val);
+    
+    return (newList);
+}

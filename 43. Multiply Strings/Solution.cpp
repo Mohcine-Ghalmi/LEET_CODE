a05998aba9091d1257e2b1e50d12baee7b6ec95b
Mohcine Ghalmi <mghalmi@e1r9p2.1337.ma>
2024-11-24 17:19:52 +0100

    working in multiply strings

---
+++ 43. Multiply Strings/Solution.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string multiply(string num1, string num2) {
+    
+}
+
+int main(void) {
+    cout << multiply("123", "456") << endl;
+
+    return (0);
+}

0eeb88cfbf576f5d3cad71d1b8dbba19f1160d41
Mohcine-Ghalmi <mohcin.ghalmi@gmail.com>
2025-05-30 18:11:32 +0100

    Update : fixing the two dir's of queens

---
+++ 51. N-Queens/Solution.cpp
@@ -0,0 +1,46 @@
+bool checkSafe(vector<vector<string>> &vec, int posx, int posy) {
+    int i, j;
+
+    for (i = 0; i < posx; i++) 
+        if (vec[i][posy] == "Q")
+            return false;
+
+    for (i = posx - 1, j = posy - 1; i >= 0 && j >= 0 ; i--, j--)
+        if (vec[i][j] == "Q")
+            return false;
+        
+    for (i = posx - 1, j = posy + 1; j <vec.size() && i >= 0; i--, j++)
+        if (vec[i][j] == "Q")
+            return false;
+    return true;
+}
+
+void QueenPlace(int r, vector<string>& board, vector<vector<string>>& res) {
+    int n = board.size();
+    if (r == n) {
+        res.push_back(board);
+        return;
+    }
+    for (int i = 0; i < n; i++) {
+        bool safe = true;
+        for (int j = 0; j < r; j++) {
+            int q_col = board[j].find('Q');
+            if (q_col == i || abs(q_col - i) == abs(j - r)) {
+                safe = false;
+                break;
+            }
+        }
+        if (safe) {
+            board[r][i] = 'Q';
+            QueenPlace(r + 1, board, res);
+            board[r][i] = '.';
+        }
+    }
+}
+
+vector<vector<string>> solveNQueens(int n) {
+    vector<string> board(n, string(n, '.'));
+    vector<vector<string>> res;
+    QueenPlace(0, board, res);
+    return res;
+}
--- 52. N-Queens II/Readme.md
+++ 52. N-Queens II/Readme.md
@@ -1,10 +1,8 @@
-# 51. N-Queens
+# 52. N-Queens II
 
 The n-queens puzzle is the problem of placing n queens on an `n x n` chessboard such that no two queens attack each other.
 
 Given an integer `n`, return the number of distinct solutions to the n-queens puzzle.
-
- 
 
 ## Example 1:
 ![alt text](image.png)
@@ -12,6 +10,7 @@ Given an integer `n`, return the number of distinct solutions to the n-queens pu
         Input: n = 4
         Output: 2
         Explanation: There are two distinct solutions to the 4-queens puzzle as shown.
+
 ## Example 2:
 
         Input: n = 1

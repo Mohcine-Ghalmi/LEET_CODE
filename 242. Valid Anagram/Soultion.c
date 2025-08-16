e1530b44d80dfda584ee924f532b45216f2061bc
Mohcin-Ghalmi <mohcin.ghalmi@gmail.com>
2025-08-16 11:34:09 +0100

    [UPDATE] : solve the probleme using sort and compare theme

---
+++ 242. Valid Anagram/Soultion.c
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+bool isAnagram(string s, string t) {
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+    return s == t;
+}
+
+int main() {
+    string s = "anagram";
+    string t = "nagaram";
+    cout << isAnagram(s, t) << endl;  // Output: 1 (true)
+    return 0;
+}
